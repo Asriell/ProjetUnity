6c752bbe05a3328807d1b0c05e5b4efa31379c5f
p1507706 <kevin.burdin@etu.univ-lyon1.fr>
2020-10-26 17:25:41 +0100

    reseau fait

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof (PlayerMotor))]
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+
+    [SerializeField]
+    private float lookSensitivityX;
+
+    [SerializeField]
+    private float lookSensitivityY;
+
+    [SerializeField]
+    private float thrusterForce = 1000;
+
+    private PlayerMotor motor;
+
+    PlayerController()
+    {
+        speed = 5;
+        lookSensitivityX = 3;
+        lookSensitivityY = 3;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        motor = GetComponent<PlayerMotor>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        float _xMove = Input.GetAxisRaw("Horizontal");
+        float _zMove = Input.GetAxisRaw("Vertical");
+
+        Vector3 _velocity = (transform.right * _xMove + transform.forward * _zMove) * speed;
+        motor.SetVelocity(_velocity);
+
+        float _yRotation = Input.GetAxisRaw("Mouse X");
+        float _xRotation = Input.GetAxisRaw("Mouse Y");
+        Vector3 _rotationX = new Vector3(0, _yRotation, 0) * lookSensitivityX;
+        Vector3 _rotationY = new Vector3(_xRotation, 0, 0) * lookSensitivityY;
+        motor.SetRotationX(_rotationX);
+        motor.SetRotationY(_rotationY);
+
+        Vector3 _thrusterForce = Vector3.zero;
+        if (Input.GetButton("Jump"))
+        {
+            _thrusterForce = Vector3.up * thrusterForce;
+        }
+        motor.SetThrusterForce(_thrusterForce);
+    }
+}
---
+++ Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMotor : MonoBehaviour
+{
+    private Camera cam;
+    private Vector3 velocity;
+    private Vector3 rotationX; //rotation selon l'axe X de la souris
+    private Vector3 rotationY; //rotation selon l'axe Y de la souris
+    private Vector3 thrusterForce;
+    private Rigidbody rb;
+
+    PlayerMotor()
+    {
+        velocity = Vector3.zero;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        cam = GetComponentInChildren<Camera>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Move();
+        Rotate();
+        Fly();
+    }
+    public void SetVelocity(Vector3 _velocity)
+    {
+        velocity = _velocity;
+    }
+
+    public void SetRotationX(Vector3 _rotationX)
+    {
+        rotationX = _rotationX;
+    }
+
+    public void SetRotationY(Vector3 _rotationY)
+    {
+        rotationY = _rotationY;
+    }
+
+    public void SetThrusterForce(Vector3 _thrusterForce)
+    {
+        thrusterForce = _thrusterForce;
+    }
+
+    private void Move()
+    {
+        if(velocity != Vector3.zero)
+        {
+            rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+        }
+    }
+
+    private void Rotate()
+    {
+        rb.MoveRotation(rb.rotation * Quaternion.Euler(rotationX));
+        cam.transform.Rotate(-rotationY);
+    }
+
+    private void Fly()
+    {
+        if (thrusterForce != Vector3.zero)
+        {
+            rb.AddForce(thrusterForce * Time.fixedDeltaTime, ForceMode.Acceleration) ;
+        }
+    }
+}
