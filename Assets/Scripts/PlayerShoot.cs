2bf7c612a6818f7d039010d012c52f95ae104578
p1507706 <kevin.burdin@etu.univ-lyon1.fr>
2020-10-28 02:53:40 +0100

    raycast beginning

---
+++ Assets/Scripts/PlayerShoot.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerShoot : NetworkBehaviour
+{
+    [SerializeField]
+    private PlayerWeapon weapon;
+
+    [SerializeField]
+    private Camera cam;
+
+    [SerializeField]
+    private LayerMask mask;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (cam == null)
+        {
+            Debug.LogError("camera not set !");
+            this.enabled = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        RaycastHit _hit;
+
+        if (Physics.Raycast(cam.transform.position, cam.transform.forward, out _hit, weapon.GetRange(), mask))
+        {
+            print("shot : " + _hit.collider.name);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerWeapon.cs
@@ -0,0 +1,45 @@
+﻿
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+[System.Serializable]
+public class PlayerWeapon
+{
+    [SerializeField]
+    private string name = "Glock";
+    [SerializeField]
+    private float damage = 10;
+    [SerializeField]
+    private float range = 100;
+
+    public string GetName()
+    {
+        return name;
+    }
+
+    public float GetDamage()
+    {
+        return damage;
+    }
+
+    public float GetRange()
+    {
+        return range;
+    }
+
+    public void SetName(string _name)
+    {
+        name = _name;
+    }
+
+    public void SetDamage(float _damage)
+    {
+        damage = _damage;
+    }
+
+    public void SetRange(float _range)
+    {
+        range = _range;
+    }
+
+}
