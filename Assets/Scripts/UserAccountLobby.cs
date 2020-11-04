2a914b9ccfd97ea3e6a8c04d6b6181568969615e
p1507706 <kevin.burdin@etu.univ-lyon1.fr>
2020-11-04 21:18:33 +0100

    gestion comptes joueur

---
+++ Assets/Scripts/UserAccountLobby.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class UserAccountLobby : MonoBehaviour
+{
+    public Text UserNameText;
+
+    private void Start()
+    {
+        if (UserAccountManager.IsLoggedIn)
+        {
+            UserNameText.text = UserAccountManager.LoggedIn_Username;
+        }
+        else
+        {
+            UserNameText.text = "Foobar";
+        }
+    }
+
+    public void LogOut()
+    {
+        if (UserAccountManager.IsLoggedIn)
+        {
+            UserAccountManager.instance.LogOut();
+        }
+    }
+}
