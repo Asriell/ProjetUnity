40011d10d52634a67eef92edd3626e8d93079623
p1507706 <kevin.burdin@etu.univ-lyon1.fr>
2020-12-02 14:27:50 +0100

    choix personnage (l'hote choisit pour toute la partie, a changer

---
+++ Assets/Scripts/UI/SelectCharacterScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+[RequireComponent (typeof(ToggleGroup))]
+public class SelectCharacterScript : MonoBehaviour
+{
+    private ToggleGroup toggleGroup;
+    private NetworkManager networkManager;
+    private Toggle PreviousSelectedToggle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        toggleGroup = GetComponent<ToggleGroup>();
+        networkManager = NetworkManager.singleton;
+        PreviousSelectedToggle = toggleGroup.ActiveToggles().ElementAt(0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Toggle CurrentSelectedToggle = toggleGroup.ActiveToggles().ElementAt(0);
+        if (PreviousSelectedToggle != CurrentSelectedToggle)
+        {
+            OnToggleChange();
+            PreviousSelectedToggle = CurrentSelectedToggle;
+        }
+    }
+
+    private void OnToggleChange()
+    {
+        Debug.Log(toggleGroup.ActiveToggles().ElementAt(0).name);
+        networkManager.playerPrefab = toggleGroup.ActiveToggles().ElementAt(0).GetComponent<DataToggle>().playerPrefab;
+    }
+}
